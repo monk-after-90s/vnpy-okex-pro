c976bd3443477c2baae4721a1a5a5103869cf252
90houlaoheshang <907333918@qq.com>
2023-04-13 14:39:19 +0800

    书写正确的运行示范脚本

--- script/run.py
+++ script/run.py
@@ -1,18 +1,29 @@
+import vnpy_crypto
+
+vnpy_crypto.init()
 from vnpy.event import EventEngine
 from vnpy.trader.engine import MainEngine
 from vnpy.trader.ui import MainWindow, create_qapp
 
-from vnpy_okex import OkexGateway
+# 上面固定不动
+# ——————————————————————————————————————————————————
+# Gateway
+from vnpy_okex_pro import OkexGateway
+# App
+from vnpy_datamanager import DataManagerApp
 
 
 def main():
     """主入口函数"""
     qapp = create_qapp()
-
     event_engine = EventEngine()
     main_engine = MainEngine(event_engine)
+    # ——————————————————————————————————————————————————
+    # Gateway
     main_engine.add_gateway(OkexGateway)
-
+    # App
+    main_engine.add_app(DataManagerApp)
+    # ——————————————————————————————————————————————————
     main_window = MainWindow(main_engine, event_engine)
     main_window.showMaximized()
 
